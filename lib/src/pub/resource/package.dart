28a0f183d2faa005089c3bad4f0a734999de0338
Joel Leibow <joel.leibow@workiva.com>
2018-11-03 12:33:31 -0700

    Do some caching of operations that may be repeated without data changing

--- lib/src/pub/resource/package.dart
+++ lib/src/pub/resource/package.dart
@@ -3,14 +3,30 @@ import 'dart:collection';
 import './package_version.dart';
 
 class PackageResource extends MapView {
+  PackageVersionResource _latestVersion;
+  List<PackageVersionResource> _versions;
+
   PackageResource(Map map) : super(map);
 
-  PackageVersionResource get latest =>
-      new PackageVersionResource(this['latest']);
+  PackageVersionResource get latest {
+    if (_latestVersion == null) {
+      _latestVersion = new PackageVersionResource(this['latest']);
+    }
+
+    return _latestVersion;
+  }
 
   String get name => this['name'];
 
-  Iterable<PackageVersionResource> get versions => this['versions']
-      .map((packageVersion) => new PackageVersionResource(packageVersion))
-      .toList();
+  List<PackageVersionResource> get versions {
+    if (_versions == null) {
+      this['versions']
+          .map((packageVersion) => new PackageVersionResource(packageVersion))
+          .toList();
+    }
+
+    return _versions;
+  }
+
+  bool get isWorkivaPackage => latest.homepage.contains('Workiva');
 }
