945d56f1bcdebda1fe1bad644233fd933a96be82
Joel Leibow <joel.leibow@workiva.com>
2018-11-03 15:15:25 -0700

    Measure Dart code metrics of a package

--- lib/pub_package_info.dart
+++ lib/pub_package_info.dart
@@ -2,19 +2,60 @@ library pub_package_info;
 
 import 'dart:async';
 
+import './src/models/workiva_repository.dart';
 import './src/pub/resource/package.dart';
 import './src/pub/service.dart';
 
 export './src/pub/resource/package.dart';
 
-Future<PackageResource> getPackageInfo(String packageName) async {
-  PubService pubService = new PubService();
+Future<PackageResource> getPackage(String packageName,
+    {String pubServerHost}) async {
+  final pubService = new PubService(pubServerHost: pubServerHost);
 
-  return await pubService.getPackageInfo(packageName);
+  return await pubService.getPackage(packageName);
+}
+
+Future<PackageResource> getWorkivaPackage(String packageName) async {
+  return await getPackage(packageName,
+      pubServerHost: 'https://pub.workiva.org');
+}
+
+Future<Map<String, int>> getWorkivaPackageMetrics(String packageName) async {
+  final package = await getWorkivaPackage(packageName);
+
+  if (package != null && package.isWorkivaPackage) {
+    final workivaRepo =
+        new WorkivaRepository(Uri.parse(package.latest.homepage));
+
+    await workivaRepo.clone();
+    return await workivaRepo.dartCodeMetrics();
+  }
+
+  return null;
 }
 
 List versionsSupportingSdkVersion(PackageResource package, String sdkVersion) {
-  PubService pubService = new PubService();
+  final pubService = new PubService();
 
   return pubService.versionsSupportingSdkVersion(package, sdkVersion);
 }
+
+// Future<List<RepositorySlug>> _getPackageRepoSlugs(
+//     List<String> packageNames) async {
+//   final pubService = new PubService(pubServerHost: 'https://pub.workiva.org');
+
+//   var packageInfo;
+//   final repoSlugs = [];
+//   for (var package in packageNames) {
+//     packageInfo = await pubService.getPackageInfo(package);
+
+//     if (packageInfo != null &&
+//         packageInfo.latest.homepage.contains('github.com/Workiva')) {
+//       // We can always assume the second index to be the repo name.
+//       repoSlugs.add(new RepositorySlug('Workiva',
+//           Uri.parse(packageInfo.latest.homepage).path.split('/')[2]));
+//     }
+//   }
+
+//   return repoSlugs;
+// }
--- lib/src/models/workiva_repository.dart
+++ lib/src/models/workiva_repository.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 import 'dart:io';
 
+import 'package:dloc/dloc_base.dart';
 import 'package:dotenv/dotenv.dart' show env, load;
 import 'package:git/git.dart';
 import 'package:github/server.dart';
@@ -8,28 +9,54 @@ import 'package:github/server.dart';
 import '../constants.dart';
 
 class WorkivaRepository {
+  Directory _checkoutDir;
+  Map<String, int> _dartCodeMetrics;
   GitHub _github;
   Repository _repo;
   RepositorySlug _repoSlug;
 
   WorkivaRepository(Uri workivaGithubUri) {
+    final repoName = workivaGithubUri.path.replaceFirst('/Workiva/', '');
     _repoSlug = new RepositorySlug(
-        'Workiva', workivaGithubUri.path.replaceFirst("/Workiva/", ""));
-
-    load();
-    _github = createGitHubClient(
-        auth: new Authentication.withToken(env['GITHUB_API_TOKEN']));
+        'Workiva', repoName.replaceFirst('/', '', (repoName.length - 1)));
   }
 
-  Future<ProcessResult> clone() async {
+  clone() async {
     await _fetchRepository();
 
-    return await runGit(['clone', '--depth', '1', _repo.cloneUrls.ssh],
-        processWorkingDir: REPOSITORY_DIRECTORY);
+    if (!(await _checkoutDir.exists())) {
+      await runGit(['clone', '--depth', '1', _repo.cloneUrls.ssh],
+          processWorkingDir: REPOSITORY_DIRECTORY);
+    }
+  }
+
+  Future<Map<String, int>> dartCodeMetrics() async {
+    if (_dartCodeMetrics == null) {
+      await clone();
+
+      initialize(new CommandLineArgs(parser.parse([_checkoutDir.path])),
+          silenceLogger: true);
+      final locResult = countLinesOfCode(_checkoutDir.path);
+      final dartCodeMetrics = locResult.data[hasMatch('main.dart').ext];
+
+      _dartCodeMetrics = {
+        'files': dartCodeMetrics.files,
+        'lines': dartCodeMetrics.lines,
+        'blanks': dartCodeMetrics.blanks,
+        'comments': dartCodeMetrics.comments
+      };
+    }
+
+    return _dartCodeMetrics;
   }
 
   _fetchRepository() async {
-    if (_repo == null)
-      _repo = await _github.repositories.getRepository(_repoSlug);
+    if (_repo == null) load();
+    _github = createGitHubClient(
+        auth: new Authentication.withToken(env['GITHUB_API_TOKEN']));
+
+    _repo = await _github.repositories.getRepository(_repoSlug);
+
+    _checkoutDir = new Directory('${REPOSITORY_DIRECTORY}/${_repo.name}');
   }
 }
