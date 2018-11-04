5c5802391725673ba6e3b2ef384818f76b70cfe6
Joel Leibow <joel.leibow@workiva.com>
2018-11-04 14:35:56 -0700

    Helper methods for gathering packages to investigate.

--- tool/wdesk_deps.dart
+++ tool/wdesk_deps.dart
@@ -1,244 +1,250 @@
 /**
- * Last pulled via pub deps -s compact from the Wdesk repo on 10/26/2018.
+ * Last pulled via pub deps -s compact from the Wdesk repo on 11/04/2018.
 */
-List<String> _internal = [
-  'admin_client',
-  'admin_frugal',
-  'app_intelligence',
-  'audit',
-  'basictracer_dart',
-  'bigsky_rest_files',
-  'bigsky_webdriver_utils',
-  'browser_storage',
-  'charts',
-  'comments_frugal',
-  'content_search_frugal',
-  'copy_frugal',
-  'copy_ui',
-  'dart_medic',
-  'dart_permissions_editor',
-  'dataset_service_api',
-  'dataset_service_ui',
-  'datatables',
-  'docmodel_frugal',
-  'drawing',
-  'fastpath_api',
-  'file_services_sdk',
-  'filing_orchestrator_api',
-  'focus',
-  'frugal',
-  'graph_api',
-  'graph_form_api',
-  'graph_ui',
-  'grc_services_frugal',
-  'history_frugal',
-  'home_frugal',
-  'home',
-  'iam_landing_page',
-  'key_binder',
-  'licensing_api',
-  'licensing_frugal',
-  'linking_frugal',
-  'linking_sdk',
-  'markup',
-  'messaging_sdk',
-  'opentracing_dart',
-  'proofread_frugal',
-  'rcal_api',
-  'review_bar',
-  'semver_audit',
-  'skaardb',
-  'sockjs_client',
-  'speedparser',
-  'sw_toolbox_dart',
-  'tasker',
-  'text_doc_client',
-  'thrift',
-  'toolbars',
-  'tour',
-  'truss',
-  'undo_redo',
-  'user_analytics',
-  'vessel',
-  'w_attachments',
-  'w_clipboard',
-  'w_comments',
-  'w_context_menu',
-  'w_dashboard_frugal',
-  'w_dashboard',
-  'w_editor_dev_tools',
-  'w_editor_properties',
-  'w_filing_api',
-  'w_filing',
-  'w_graph_client',
-  'w_history',
-  'w_input_validation',
-  'w_link_properties_ui',
-  'w_oauth2',
-  'w_outline',
-  'w_project',
-  'w_router',
-  'w_session',
-  'w_sox',
-  'w_table',
-  'w_test_tools',
-  'w_translate',
-  'w_user_color_module',
-  'w_viewer',
-  'w_virtual_components',
-  'w_webdriver_utils',
-  'wContent_frugal',
-  'wdesk_login',
-  'wdesk_sdk',
-  'web_skin_dart',
-  'web_skin',
-  'workflow_client',
-  'workflow_frugal',
-  'workiva_scripts',
-  'workspaces_api',
-  'workspaces_components',
-  'workspaces_frugal',
-  'wuri_sdk',
-  'xbrl_config_api',
-  'xbrl_importer_api',
-  'xbrl_module',
-  'xbrl_orchestrator_api',
-  'xbrl2_server_api',
-];
+final _private = {
+  'admin_client': {},
+  'admin_frugal': {},
+  'app_intelligence': {},
+  'audit': {},
+  'basictracer_dart': {},
+  'bigsky_rest_files': {},
+  'bigsky_webdriver_utils': {},
+  'browser_storage': {
+    'repository': 'https://github.com/Workiva/browser-storage'
+  },
+  'charts': {},
+  'comments_frugal': {},
+  'content_search_frugal': {},
+  'copy_frugal': {},
+  'copy_ui': {},
+  'dart_medic': {},
+  'dart_permissions_editor': {},
+  'dataset_service_api': {},
+  'dataset_service_ui': {},
+  'datatables': {},
+  'docmodel_frugal': {},
+  'drawing': {},
+  'fastpath_api': {},
+  'file_services_sdk': {},
+  'filing_orchestrator_api': {},
+  'focus': {},
+  'frugal': {'repository': 'https://github.com/Workiva/frugal'},
+  'graph_api': {},
+  'graph_form_api': {},
+  'graph_ui': {},
+  'grc_services_frugal': {},
+  'history_frugal': {},
+  'home': {},
+  'home_frugal': {},
+  'iam_landing_page': {},
+  'key_binder': {},
+  'licensing_api': {},
+  'licensing_frugal': {},
+  'linking_frugal': {},
+  'linking_sdk': {},
+  'markup': {},
+  'messaging_sdk': {'repository': 'https://github.com/Workiva/messaging-sdk'},
+  'opentracing_dart': {},
+  'proofread_frugal': {},
+  'rcal_api': {},
+  'review_bar': {},
+  'semver_audit': {},
+  'skaardb': {},
+  'sockjs_client': {
+    'isGitDependency': true,
+    'repository': 'https://github.com/Workiva/sockjs-dart-client',
+  },
+  'speedparser': {},
+  'sw_toolbox_dart': {},
+  'tasker': {},
+  'text_doc_client': {},
+  'thrift': {},
+  'toolbars': {},
+  'tour': {},
+  'truss': {},
+  'undo_redo': {},
+  'user_analytics': {},
+  'vessel': {},
+  'w_attachments': {},
+  'w_clipboard': {},
+  'w_comments': {},
+  'w_context_menu': {},
+  'w_dashboard_frugal': {},
+  'w_dashboard': {},
+  'w_editor_dev_tools': {},
+  'w_editor_properties': {},
+  'w_filing_api': {},
+  'w_filing': {},
+  'w_graph_client': {},
+  'w_history': {},
+  'w_input_validation': {},
+  'w_link_properties_ui': {},
+  'w_oauth2': {},
+  'w_outline': {},
+  'w_project': {},
+  'w_router': {},
+  'w_session': {},
+  'w_sox': {},
+  'w_table': {},
+  'w_test_tools': {},
+  'w_translate': {},
+  'w_user_color_module': {},
+  'w_viewer': {},
+  'w_virtual_components': {},
+  'w_webdriver_utils': {},
+  'wContent_frugal': {},
+  'wdesk_login': {'repository': 'https://github.com/Workiva/wdesk_login'},
+  'wdesk_sdk': {},
+  'web_skin_dart': {},
+  'web_skin': {},
+  'workflow_client': {},
+  'workflow_frugal': {},
+  'workiva_scripts': {},
+  'workspaces_api': {},
+  'workspaces_components': {},
+  'workspaces_frugal': {},
+  'wuri_sdk': {},
+  'xbrl_config_api': {},
+  'xbrl_importer_api': {},
+  'xbrl_module': {},
+  'xbrl_orchestrator_api': {},
+  'xbrl2_server_api': {},
+};
 
-List<String> _public = [
-  'analyzer',
-  'ansicolor',
-  'archive',
-  'args',
-  'async',
-  'barback',
-  'boolean_selector',
-  'browser',
-  'build',
-  'build_barback',
-  'build_config',
-  'build_runner',
-  'built_collection',
-  'built_redux',
-  'built_value',
-  'built_value_generator',
-  'charcode',
-  'cli_util',
-  'code_transformers',
-  'collection',
-  'color',
-  'colorize',
-  'convert',
-  'cookie',
-  'coverage',
-  'crc32',
-  'crypto',
-  'csslib',
-  'csv',
-  'dart_dev',
-  'dart_style',
-  'dart2_constant',
-  'dependency_validator',
-  'dnd',
-  'faker',
-  'fixnum',
-  'fluri',
-  'font_face_observer',
-  'front_end',
-  'func',
-  'glob',
-  'highcharts',
-  'html',
-  'http',
-  'http_multi_server',
-  'http_parser',
-  'image',
-  'intl',
-  'io',
-  'isolate',
-  'js',
-  'json_annotation',
-  'json_rpc_2',
-  'kernel',
-  'logging',
-  'markdown',
-  'matcher',
-  'memoize',
-  'meta',
-  'mime',
-  'mockable_filesystem',
-  'mockito',
-  'multi_server_socket',
-  'node_preamble',
-  'opentracing',
-  'over_react',
-  'over_react_test',
-  'package_config',
-  'package_resolver',
-  'path',
-  'pdfjs',
-  'petitparser',
-  'platform_detect',
-  'plugin',
-  'pool',
-  'protobuf',
-  'pub_semver',
-  'quiver',
-  'quiver_hashcode',
-  'r_tree',
-  'react',
-  'redux',
-  'redux_logging',
-  'resource',
-  'rxdart',
-  'sass',
-  'sass_builder',
-  'scratch_space',
-  'shelf',
-  'shelf_packages_handler',
-  'shelf_proxy',
-  'shelf_static',
-  'shelf_web_socket',
-  'sockjs_client_wrapper',
-  'source_gen',
-  'source_map_stack_trace',
-  'source_maps',
-  'source_span',
-  'stack_trace',
-  'state_machine',
-  'stream_channel',
-  'stream_transform',
-  'string_scanner',
-  'supports_color',
-  'synchronized',
-  'term_glyph',
-  'test',
-  'transformer_utils',
-  'tuple',
-  'typed_data',
-  'unittest',
-  'unscripted',
-  'utf',
-  'uuid',
-  'vm_service_client',
-  'w_common',
-  'w_flux',
-  'w_module',
-  'w_transport',
-  'watcher',
-  'web_socket_channel',
-  'webdriver',
-  'when',
-  'which',
-  'xml',
-  'yaml',
-];
+final _public = {
+  'analyzer': {},
+  'ansicolor': {},
+  'archive': {},
+  'args': {},
+  'async': {},
+  'barback': {},
+  'boolean_selector': {},
+  'browser': {},
+  'build_barback': {},
+  'build_config': {},
+  'build_runner': {},
+  'build': {},
+  'built_collection': {},
+  'built_redux': {},
+  'built_value_generator': {},
+  'built_value': {},
+  'charcode': {},
+  'cli_util': {},
+  'code_transformers': {},
+  'collection': {},
+  'color': {},
+  'colorize': {},
+  'convert': {},
+  'cookie': {},
+  'coverage': {},
+  'crc32': {},
+  'crypto': {},
+  'csslib': {},
+  'csv': {},
+  'dart_dev': {'isPublicWorkivaPackage': true},
+  'dart_style': {},
+  'dart_to_js_script_rewriter': {'isPublicWorkivaPackage': true},
+  'dart2_constant': {},
+  'dependency_validator': {'isPublicWorkivaPackage': true},
+  'dnd': {},
+  'faker': {},
+  'fixnum': {},
+  'fluri': {'isPublicWorkivaPackage': true},
+  'font_face_observer': {'isPublicWorkivaPackage': true},
+  'front_end': {},
+  'func': {},
+  'glob': {},
+  'highcharts': {},
+  'html': {},
+  'http_multi_server': {},
+  'http_parser': {},
+  'http': {},
+  'image': {},
+  'intl': {},
+  'io': {},
+  'isolate': {},
+  'js': {},
+  'json_annotation': {},
+  'json_rpc_2': {},
+  'kernel': {},
+  'logging': {},
+  'markdown': {},
+  'matcher': {},
+  'memoize': {},
+  'meta': {},
+  'mime': {},
+  'mockable_filesystem': {},
+  'mockito': {},
+  'multi_server_socket': {},
+  'node_preamble': {},
+  'opentracing': {},
+  'over_react_test': {'isPublicWorkivaPackage': true},
+  'over_react': {'isPublicWorkivaPackage': true},
+  'package_config': {},
+  'package_resolver': {},
+  'path': {},
+  'pdfjs': {'isPublicWorkivaPackage': true},
+  'petitparser': {},
+  'platform_detect': {'isPublicWorkivaPackage': true},
+  'plugin': {},
+  'pool': {},
+  'protobuf': {},
+  'pub_semver': {},
+  'quiver_hashcode': {},
+  'quiver': {},
+  'r_tree': {'isPublicWorkivaPackage': true},
+  'react': {},
+  'redux_logging': {},
+  'redux': {},
+  'resource': {},
+  'rxdart': {},
+  'sass_builder': {},
+  'sass': {},
+  'scratch_space': {},
+  'shelf_packages_handler': {},
+  'shelf_proxy': {},
+  'shelf_static': {},
+  'shelf_web_socket': {},
+  'shelf': {},
+  'sockjs_client_wrapper': {'isPublicWorkivaPackage': true},
+  'source_gen': {},
+  'source_map_stack_trace': {},
+  'source_maps': {},
+  'source_span': {},
+  'stack_trace': {},
+  'state_machine': {'isPublicWorkivaPackage': true},
+  'stream_channel': {},
+  'stream_transform': {},
+  'string_scanner': {},
+  'supports_color': {},
+  'synchronized': {},
+  'term_glyph': {},
+  'test': {},
+  'transformer_utils': {'isPublicWorkivaPackage': true},
+  'tuple': {},
+  'typed_data': {},
+  'unittest': {},
+  'unscripted': {},
+  'utf': {},
+  'uuid': {},
+  'vm_service_client': {},
+  'w_common': {'isPublicWorkivaPackage': true},
+  'w_flux': {'isPublicWorkivaPackage': true},
+  'w_module': {'isPublicWorkivaPackage': true},
+  'w_transport': {'isPublicWorkivaPackage': true},
+  'watcher': {},
+  'web_socket_channel': {},
+  'webdriver': {},
+  'when': {},
+  'which': {},
+  'xml': {},
+  'yaml': {},
+};
 /**
  * end of pub deps -s compact pull.
 */
 
-List<String> _deprecated = [
+final _deprecated = [
   'barback',
   'browser',
   'code_transformers',
@@ -247,30 +253,121 @@ List<String> _deprecated = [
 ];
 
 // Last pulled via https://datastudio.google.com/reporting/17zuxuZgMWxYqGwKS7UQAFr83tf9wShWJ/page/RDCa 10/31/2018.
-List<String> _compatibilityKnown = [
-  'abide',
-  'browser-storage',
-  'dart_dev',
-  'dependency_validator',
-  'fluri',
-  'platform_detect',
-  'sockjs-dart-client',
-  'sockjs_client_wrapper',
-  'state_machine',
-  'w_common',
-  'w_flux',
-  'w_transport',
-  'opentracing_dart',
-  'focus.dart',
-  'font_face_observer',
-  'pdfjs_dart',
-  'sw_toolbox_dart',
-];
+final _internalDart2CompatibilePackages = {
+  'browser_storage': {
+    'dart2PullRequestUri': 'https://github.com/Workiva/browser-storage/pull/30'
+  },
+  'dart_dev': {
+    'dart2PullRequestUri': 'https://github.com/Workiva/dart_dev/pull/274'
+  },
+  'dependency_validator': {
+    'dart2PullRequestUri':
+        'https://github.com/Workiva/dependency_validator/pull/42'
+  },
+  'fluri': {'dart2PullRequestUri': 'https://github.com/Workiva/fluri/pull/74'},
+  'platform_detect': {
+    'dart2PullRequestUri': 'https://github.com/Workiva/platform_detect/pull/34'
+  },
+  'sockjs_client': {
+    'dart2PullRequestUri':
+        'https://github.com/Workiva/sockjs-dart-client/pull/20'
+  },
+  'sockjs_client_wrapper': {
+    'dart2PullRequestUri':
+        'https://github.com/Workiva/sockjs_client_wrapper/pull/14',
+  },
+  'state_machine': {
+    'dart2PullRequestUri': 'https://github.com/Workiva/state_machine/pull/47',
+  },
+  'w_common': {
+    'dart2PullRequestUri': 'https://github.com/Workiva/w_common/pull/98',
+  },
+  'w_flux': {
+    'dart2PullRequestUri': 'https://github.com/Workiva/w_flux/pull/123',
+  },
+  'w_transport': {
+    'dart2PullRequestUri': 'https://github.com/Workiva/w_transport/pull/320',
+  },
+  'opentracing_dart': {
+    'dart2PullRequestUri':
+        'https://github.com/Workiva/opentracing_dart/pull/27',
+  },
+  'font_face_observer': {
+    'dart2PullRequestUri':
+        'https://github.com/Workiva/font_face_observer/pull/38',
+  },
+  'sw_toolbox_dart': {
+    'dart2PullRequestUri': 'https://github.com/Workiva/sw_toolbox_dart/pull/5',
+  },
+};
 
-var wdeskPublicDependencies = (_public.toSet()
-      ..removeAll(_deprecated)
-      ..removeAll(_compatibilityKnown))
-    .toList();
+_mergePackageConfigs(Map packages) {
+  final packageMap = new Map.from(packages);
 
-var wdeskInternalDependencies =
-    (_internal.toSet()..removeAll(_compatibilityKnown)).toList();
+  _internalDart2CompatibilePackages.forEach((packageName, packageConfig) {
+    if (packageMap.containsKey(packageName)) {
+      packageMap[packageName] = new Map.from(packageMap[packageName])
+        ..addAll(packageConfig);
+    }
+  });
+
+  return packageMap;
+}
+
+_removeDeprecatedPackages(Map packages) {
+  final packageMap = new Map.from(packages);
+
+  _deprecated.forEach((packageName) => packageMap.remove(packageName));
+
+  return packageMap;
+}
+
+_removeInternalDart2CompatibilePackages(Map packages) {
+  final packageMap = new Map.from(packages);
+
+  _internalDart2CompatibilePackages
+      .forEach((packageName, _) => packageMap.remove(packageName));
+
+  return packageMap;
+}
+
+getWdeskDependencies({withDeprecated: true}) {
+  final wdeskDependencies =
+      getPublicWdeskDependencies(withDeprecated: withDeprecated);
+  final wdeskPrivateDependencies =
+      getPrivateWdeskDependencies(withDeprecated: withDeprecated);
+  wdeskDependencies.addAll(wdeskPrivateDependencies);
+
+  return wdeskDependencies;
+}
+
+getPublicWdeskDependencies({withDeprecated: true}) {
+  return _mergePackageConfigs(
+      (withDeprecated) ? _public : _removeDeprecatedPackages(_public));
+}
+
+getPrivateWdeskDependencies({withDeprecated: true}) {
+  return _mergePackageConfigs(
+      (withDeprecated) ? _private : _removeDeprecatedPackages(_private));
+}
+
+getInternalWdeskDependencies(
+    {withDeprecated: true, withInternalDart2CompatibilePackages: true}) {
+  final publicWdeskDependencies =
+      getPublicWdeskDependencies(withDeprecated: withDeprecated);
+  final internalWdeskDependencies = new Map();
+
+  publicWdeskDependencies.forEach((packageName, packageConfig) {
+    if (packageConfig['isPublicWorkivaPackage'] != null &&
+        packageConfig['isPublicWorkivaPackage'])
+      internalWdeskDependencies[packageName] = packageConfig;
+  });
+
+  internalWdeskDependencies
+      .addAll(getPrivateWdeskDependencies(withDeprecated: withDeprecated));
+
+  return (withInternalDart2CompatibilePackages)
+      ? internalWdeskDependencies
+      : _removeInternalDart2CompatibilePackages(
+          getPrivateWdeskDependencies(withDeprecated: withDeprecated));
+}
