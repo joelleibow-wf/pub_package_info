9aa34df1c8a0dda50f01c781c7c096d5a2ec5b83
Joel Leibow <joel.leibow@workiva.com>
2018-11-05 11:03:23 -0700

    This should of been in the last commit

--- lib/src/workiva_package_repository/service.dart
+++ lib/src/workiva_package_repository/service.dart
@@ -1,26 +1,44 @@
 import 'dart:async';
+import 'dart:io';
 
 import 'package:dloc/dloc_base.dart';
+import 'package:git/git.dart';
 
+import '../constants.dart';
+import '../pub/resource/package.dart';
 import './model/workiva_package_dart_metrics.dart';
 import './model/workiva_package_repository.dart';
-import '../pub/resource/package.dart';
 
 class WorkivaPackageRepositoryService {
-  WorkivaRepository _repository;
+  WorkivaRepository _workivaRepository;
   PackageResource _package;
 
   WorkivaPackageRepositoryService(this._package) {
-    _repository = new WorkivaRepository(Uri.parse(_package.repositoryUri));
+    _workivaRepository =
+        new WorkivaRepository(Uri.parse(_package.repositoryUri));
+  }
+
+  Future<Directory> checkout() async {
+    if (!_workivaRepository.isLoaded) _workivaRepository.fetch();
+    _workivaRepository.checkoutDirectory =
+        new Directory('${REPOSITORY_DIRECTORY}/${_workivaRepository.name}');
+
+    if (!_workivaRepository.hasCheckout) {
+      await runGit(['clone', '--depth', '1', _workivaRepository.cloneUrls.ssh],
+          processWorkingDir: REPOSITORY_DIRECTORY);
+    }
+
+    return _workivaRepository.checkoutDirectory;
   }
 
   Future<WorkivaPackageDartMetrics> getDartCodeMetrics() async {
-    if (!_repository.isCloned) await _repository.clone();
+    final _repoDirPath = (!_workivaRepository.hasCheckout)
+        ? (await checkout()).path
+        : _workivaRepository.checkoutDirectory.path;
 
-    initialize(
-        new CommandLineArgs(parser.parse([_repository.cloneDirectory.path])),
+    initialize(new CommandLineArgs(parser.parse([_repoDirPath])),
         silenceLogger: true);
-    final locResult = countLinesOfCode(_repository.cloneDirectory.path);
+    final locResult = countLinesOfCode(_repoDirPath);
     final dartCodeMetrics = locResult.data[hasMatch('main.dart').ext];
 
     return new WorkivaPackageDartMetrics(_package, {
