945b0290bd1d8acdff9877de1992bda492ded30a
Joel Leibow <joel.leibow@workiva.com>
2018-11-05 15:46:08 -0600

    Get pull request data from github

--- lib/src/workiva_package_repository/service.dart
+++ lib/src/workiva_package_repository/service.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 
 import 'package:dloc/dloc_base.dart';
 import 'package:git/git.dart';
+import 'package:github/server.dart';
 
 import '../constants.dart';
 import '../pub/resource/package.dart';
@@ -10,31 +11,31 @@ import './model/workiva_package_dart_metrics.dart';
 import './model/workiva_package_repository.dart';
 
 class WorkivaPackageRepositoryService {
-  WorkivaRepository _workivaRepository;
+  WorkivaRepository _packageRepository;
   PackageResource _package;
 
   WorkivaPackageRepositoryService(this._package) {
-    _workivaRepository =
+    _packageRepository =
         new WorkivaRepository(Uri.parse(_package.repositoryUri));
   }
 
   Future<Directory> checkout() async {
-    if (!_workivaRepository.isLoaded) _workivaRepository.fetch();
-    _workivaRepository.checkoutDirectory =
-        new Directory('${REPOSITORY_DIRECTORY}/${_workivaRepository.name}');
+    await _fetchRepositoryData();
+    _packageRepository.checkoutDirectory =
+        new Directory('${REPOSITORY_DIRECTORY}/${_packageRepository.name}');
 
-    if (!_workivaRepository.hasCheckout) {
-      await runGit(['clone', '--depth', '1', _workivaRepository.cloneUrls.ssh],
+    if (!_packageRepository.hasCheckout) {
+      await runGit(['clone', '--depth', '1', _packageRepository.cloneUrls.ssh],
           processWorkingDir: REPOSITORY_DIRECTORY);
     }
 
-    return _workivaRepository.checkoutDirectory;
+    return _packageRepository.checkoutDirectory;
   }
 
   Future<WorkivaPackageDartMetrics> getDartCodeMetrics() async {
-    final _repoDirPath = (!_workivaRepository.hasCheckout)
+    final _repoDirPath = (!_packageRepository.hasCheckout)
         ? (await checkout()).path
-        : _workivaRepository.checkoutDirectory.path;
+        : _packageRepository.checkoutDirectory.path;
 
     initialize(new CommandLineArgs(parser.parse([_repoDirPath])),
         silenceLogger: true);
@@ -47,5 +48,16 @@ class WorkivaPackageRepositoryService {
       'blanks': dartCodeMetrics.blanks,
       'comments': dartCodeMetrics.comments
     });
+  }
+
+  Future<PullRequest> getPullRequestData(String pullRequestUri) async {
+    await _fetchRepositoryData();
+
+    return await _packageRepository.fetchPullRequest(
+        int.parse(Uri.parse(pullRequestUri).path.split('/').last));
+  }
+
+  _fetchRepositoryData() async {
+    if (!_packageRepository.isLoaded) await _packageRepository.fetch();
   }
 }
